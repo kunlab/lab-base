aa3b169798cdf6832ce2429a7b075557661fa6c8
kun <2206391776@qq.com>
2021-04-16 18:23:03 +0800

    定义基础异常类

---
+++ src/main/java/com/kunlab/core/exception/BaseException.java
@@ -0,0 +1,24 @@
+package com.kunlab.core.exception;
+
+/**
+ * 基础异常类，所有自定义业务异常类都需要继承此类
+ * @author likun
+ * @date 2021/4/16
+ */
+public class BaseException extends Exception{
+
+    public BaseException() {
+    }
+
+    public BaseException(String message) {
+        super(message);
+    }
+
+    public BaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BaseException(Throwable cause) {
+        super(cause);
+    }
+}
