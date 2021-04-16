e01a67b915bdf71d5e4de7742740f0791d70aec5
kun <2206391776@qq.com>
2021-04-16 18:55:06 +0800

    自定义异常类

---
+++ src/main/java/com/kunlab/core/exception/AppException.java
@@ -0,0 +1,48 @@
+package com.kunlab.core.exception;
+
+import com.kunlab.core.constant.IResultCodeEnum;
+
+/**
+ * 业务异常类
+ * @author likun
+ * @date 2021/4/16
+ */
+public class AppException extends BaseException{
+
+    //错误码
+    private String code;
+
+    //错误描述
+    private String msg;
+
+    public AppException(String code) {
+        super(code);
+        this.code = code;
+    }
+
+    public AppException(String code, String msg) {
+        super(String.format("%s : %s", code, msg));
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public AppException(Throwable cause, String code, String msg) {
+        super(String.format("%s : %s", code, msg), cause);
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public AppException(IResultCodeEnum errorEnum) {
+        super(String.format("%s : %s", errorEnum.getCode(), errorEnum.getDesc()));
+        this.code = errorEnum.getCode();
+        this.msg = errorEnum.getDesc();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
