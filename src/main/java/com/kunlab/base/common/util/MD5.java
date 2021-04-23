b59757aac10bcfbcbc1c9de3f48d05ad9271e090
likun <2206391776@qq.com>
2021-04-23 23:52:02 +0800

    优化

--- src/main/java/com/kunlab/base/common/util/MD5.java
+++ src/main/java/com/kunlab/base/common/util/MD5.java
@@ -1,33 +1,48 @@
 package com.kunlab.base.common.util;
 
-import com.sun.istack.internal.NotNull;
-
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
+ * MD5计算信息摘要
  * @author likun
  * @date 2021/4/21
  */
 public class MD5 {
+
     public static final String DEFAULT_CHARSET			= "UTF-8";
+    public static final String DEFAULT_ALGORITHM        = "MD5";
 
     /**
-     * md5加密
-     * @param bytes 待加密内容
-     * @param toUpperCase 是否转化为大写输出
-     * @return hashCode
+     * 根据指定的算法和输入计算散列码
+     * @param input 待计算散列码数据
+     * @param algorithm the name of the algorithm requested. 支持 MD5/SHA-1/SHA-256
+     * @return byteSource, 进而可以转化为 Hex,
+     * @throws NoSuchAlgorithmException if no Provider supports a
+     *          MessageDigestSpi implementation for the
+     *          specified algorithm.
      */
-    public static String md5(@NotNull byte[] bytes, boolean toUpperCase) {
-        MessageDigest messageDigest = null;
+    public static byte[] digest(byte[] input, String algorithm) throws NoSuchAlgorithmException {
+        //获取MessageDigest实例
+        MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+        //添加需要计算散列的数据
+        messageDigest.update(input);
+        //生成散列码
+        return messageDigest.digest();
+    }
+
+    /**
+     * md5信息摘要
+     * @param bytes 待计算散列码数据
+     * @param toUpperCase 是否转化为大写输出
+     * @return HexString
+     */
+    public static String md5(byte[] bytes, boolean toUpperCase) {
         try {
-            messageDigest = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            // no happen
+            bytes = digest(bytes, DEFAULT_ALGORITHM);
+        } catch (NoSuchAlgorithmException ignore) {
         }
-        messageDigest.update(bytes);
-        bytes = messageDigest.digest();
 
         StringBuilder sb = new StringBuilder();
         for(byte b : bytes) {
@@ -42,22 +57,22 @@ public class MD5 {
     }
 
     /**
-     * MD5加密
-     * @param bytes 待加密内容
-     * @return 默认输出大写hashCode
+     * MD5信息摘要
+     * @param bytes 待计算散列码数据
+     * @return HexString
      */
-    public static String md5(@NotNull byte[] bytes) {
-        return md5(bytes, true);
+    public static String md5(byte[] bytes) {
+        return md5(bytes, false);
     }
 
     /**
-     * MD5加密
-     * @param text 待加密内容
-     * @param key 加密密钥
+     * MD5信息摘要
+     * @param text 待计算散列码数据
+     * @param key 给text增加的密钥成分
      * @param toUpperCase 是否转化为大写输出
-     * @return hashCode
+     * @return HexString
      */
-    public static String md5(@NotNull byte[] text, byte[] key, boolean toUpperCase) {
+    public static String md5(byte[] text, byte[] key, boolean toUpperCase) {
         byte[] bytes = new byte[text.length + key.length];
         System.arraycopy(text, 0, bytes, 0, text.length);
         System.arraycopy(key, 0, bytes, text.length, key.length);
@@ -66,126 +81,135 @@ public class MD5 {
     }
 
     /**
-     * MD5加密
-     * @param text 待加密内容
-     * @param key 加密密钥
-     * @return 默认返回大写hashcode
+     * MD5信息摘要
+     * @param text 待计算散列码数据
+     * @param key 给text增加的密钥成分
+     * @return HexString
      */
-    public static String md5(@NotNull byte[] text, byte[] key) {
-        return md5(text, key, true);
+    public static String md5(byte[] text, byte[] key) {
+        return md5(text, key, false);
     }
 
     /**
-     * MD5加密,指定字符编码
-     * @param text 加密内容
-     * @param key 加密密钥
+     * MD5信息摘要, 指定字符编码
+     * @param text 待计算散列码数据
+     * @param key 给text增加的密钥成分
      * @param charsetName
      *        The name of a supported {@linkplain java.nio.charset.Charset
      *         charset}
      * @param toUpperCase 是否大写输出
-     * @return hashCode
+     * @return HexString
      * @throws UnsupportedEncodingException
      *         If the named charset is not supported
      *
      */
-    public static String md5(@NotNull String text, String key, String charsetName, boolean toUpperCase) throws UnsupportedEncodingException {
+    public static String md5(String text, String key, String charsetName, boolean toUpperCase) throws UnsupportedEncodingException {
         byte[] bytes = (text + key).getBytes(charsetName);
         return md5(bytes, toUpperCase);
     }
 
     /**
-     * MD5加密，默认返回大写hashCode
-     * @param text 加密内容
-     * @param key 加密密钥
+     * MD5信息摘要, 指定字符编码
+     * @param text 待计算散列码数据
+     * @param key 给text增加的密钥成分
      * @param charsetName
      *        The name of a supported {@linkplain java.nio.charset.Charset
      *         charset}
-     * @return 大写hashCode
+     * @return HexString
      * @throws UnsupportedEncodingException
      *         If the named charset is not supported
      */
-    public static String md5(@NotNull String text, String key, String charsetName) throws UnsupportedEncodingException {
-        return md5(text, key, charsetName, true);
+    public static String md5(String text, String key, String charsetName) throws UnsupportedEncodingException {
+        return md5(text, key, charsetName, false);
     }
 
     /**
-     * 默认字符编码（UTF-8）的 MD5加密
-     * @param text 加密内容
-     * @param key 加密密钥
-     * @return 大写hashCode
+     * 指定字符编码的 MD5信息摘要
+     * @param text 待计算散列码数据
+     * @param charsetName
+     *        The name of a supported {@linkplain java.nio.charset.Charset
+     *         charset}
+     * @return HexString
      */
-    public static String md5(@NotNull String text, String key) {
+    public static String md5(String text, String charsetName) throws UnsupportedEncodingException{
+        return md5(text, "", charsetName, false);
+    }
+
+    /**
+     * 默认字符编码（UTF-8）的 MD5信息摘要
+     * @param text 待计算散列码数据
+     * @return HexString
+     */
+    public static String md5(String text) {
         try {
-            return md5(text, key, DEFAULT_CHARSET, true);
+            return md5(text, "", DEFAULT_CHARSET, false);
         } catch (UnsupportedEncodingException ignore) {
             //no happen
             return null;
         }
     }
 
-    /**
-     * MD5 加密
-     * @param text 加密内容
-     * @return hashCode
-     */
-    public static String md5(@NotNull String text) {
-        try {
-            return md5(text, "", DEFAULT_CHARSET, true);
-        } catch (UnsupportedEncodingException ignore) {
-            //no happen
-            return null;
-        }
-    }
+
 
 
 
 
     /**
      * 验证MD5
-     * @param bytes 待加密原文
-     * @param md5 待验证的hashcode
+     * @param bytes 待计算散列码数据
+     * @param md5 待验证的HexString
      * @return true：通过； false:未通过
      */
-    public static boolean verify(@NotNull byte[] bytes, String md5) {
+    public static boolean verify(byte[] bytes, String md5) {
         return md5.equalsIgnoreCase(md5(bytes));
     }
 
     /**
      * 验证MD5
-     * @param text 待加密原文
+     * @param text 待计算散列码数据
      * @param key 加密密钥
-     * @param md5 待验证hashcode
+     * @param md5 待验证的HexString
      * @return true: 通过； false:未通过
      */
-    public static boolean verify(@NotNull byte[] text, byte[] key, String md5) {
+    public static boolean verify(byte[] text, byte[] key, String md5) {
         return md5.equalsIgnoreCase(md5(text, key));
     }
 
     /**
      * 验证MD5
-     * @param text 待加密的原文
-     * @param key 可选的加密密钥
+     * @param text 待计算散列码数据
+     * @param key 加密密钥
      * @param charsetName md5计算时使用的字符编码
      *         The name of a supported {@linkplain java.nio.charset.Charset
      *         charset}
-     * @param md5 待验证的hashcode
+     * @param md5 待验证的HexString
      * @return true:通过； false:未通过
      * @throws UnsupportedEncodingException
      *         If the named charset is not supported
      */
-    public static boolean verify(@NotNull String text, String key, String charsetName, String md5) throws UnsupportedEncodingException {
+    public static boolean verify(String text, String key, String charsetName, String md5) throws UnsupportedEncodingException {
         return md5.equalsIgnoreCase(md5(text, key, charsetName));
     }
 
     /**
      * 验证MD5, MD5的计算默认采用UTF-8编码
-     * @param text 待加密数据原文
-     * @param key 加密密钥
-     * @param md5 待验证 hashcode
+     * @param text 待计算散列码数据
+     * @param charsetName 加密密钥
+     * @param md5 待验证的HexString
      * @return true:通过； false:未通过
      */
-    public static boolean verify(@NotNull String text, String key, String md5){
-        return md5.equalsIgnoreCase(md5(text, key));
+    public static boolean verify(String text, String charsetName, String md5) throws UnsupportedEncodingException {
+        return md5.equalsIgnoreCase(md5(text, charsetName));
+    }
+
+    /**
+     * 验证MD5
+     * @param text 待计算散列码数据
+     * @param md5 待验证的HexString
+     * @return true:通过； false:未通过
+     */
+    public static boolean verify(String text, String md5) {
+        return md5.equalsIgnoreCase(md5(text));
     }
 
 }
