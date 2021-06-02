fa104d298109d8e407a10bfc24472ecc71370da2
ikun <943265266@qq.com>
2021-06-02 10:53:36 +0800

    字符串True False 表示

--- src/main/java/com/kunlab/base/core/constant/Constants.java
+++ src/main/java/com/kunlab/base/core/constant/Constants.java
@@ -10,9 +10,13 @@ public class Constants {
 
     public static final String DEFAULT_CHARSET			= "UTF-8";			// 默认字符集
 
+    public static final String FALSE					= "0";              // 假
+    public static final String TRUE						= "1";              // 真
 
     public static final String SUCCESS                  = "success";        // 成功
     public static final String RESULT_SUCCESS			= "00000000";		// 响应成功
+
+
 
 
 }
